3f5aa13e7064c23fc4306299bb18c8517aa3f04a
Chal <134571490+clgomez5@users.noreply.github.com>
2023-07-27 19:45:54 -0700

    updated buttons

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@ var quoteOneBox = document.getElementById("quote-one");
 
 function generateQuote() {
     document.getElementById("quote-one").innerHTML = "";
-    document.getElementById("quote-two").innerHTML = "";
 
     fetch(quoteOne)
     .then(function (response) {
@@ -17,24 +16,25 @@ function generateQuote() {
         quoteOneAuthor.textContent = data.author;
         quoteOneBox.append(quoteOneLine, quoteOneAuthor);
     })
-
-    var quoteTwo = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
-    var quoteTwoBox = document.getElementById("quote-two");
-
-    fetch(quoteTwo)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-
-        var randomNumber = Math.floor(Math.random() * data.length);
-        var randomDesignQuote = data[randomNumber];
-
-        var quoteTwoLine = document.createElement("h2");
-        var quoteTwoAuthor = document.createElement("p");
-        quoteTwoLine.textContent = randomDesignQuote.content.rendered;
-        quoteTwoAuthor.textContent = randomDesignQuote.title.rendered;
-        quoteTwoBox.append(quoteTwoLine, quoteTwoAuthor);
-    })
 }
+
+
+//     var quoteTwo = "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand";
+//     var quoteTwoBox = document.getElementById("quote-two");
+
+//     fetch(quoteTwo)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+
+//         var randomNumber = Math.floor(Math.random() * data.length);
+//         var randomDesignQuote = data[randomNumber];
+
+//         var quoteTwoLine = document.createElement("h2");
+//         var quoteTwoAuthor = document.createElement("p");
+//         quoteTwoLine.textContent = randomDesignQuote.content.rendered;
+//         quoteTwoAuthor.textContent = randomDesignQuote.title.rendered;
+//         quoteTwoBox.append(quoteTwoLine, quoteTwoAuthor);
+//     })
